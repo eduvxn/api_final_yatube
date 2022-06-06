76709d0c3016be81f70596b067115c92b44fabe5
Ксения Малютина <eduvxn@ya.ru>
2022-06-06 17:55:26 +0300

    new try

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -2,7 +2,9 @@ from rest_framework import permissions
 
 
 class IsAuthorOrReadOnly(permissions.BasePermission):
-    """Проверка соответствия авторов объекта и запроса для небезопасных методов"""
+    """Проверка соответствия авторов объекта
+    и запроса для небезопасных методов
+    """
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
